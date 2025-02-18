f493a4803f0b85ffb5063653e431bc4138ac04c8
fgotelip <felipegotdelga@gmail.com>
2025-02-17 23:14:56 -0300

    teste boladao

--- Code/e2.py
+++ Code/e2.py
@@ -3,19 +3,19 @@ from Retangulo import Retangulo
 from Tensao import Tensao
 
 
-carregamentos1 = [ Carregamento(0,3,"100*x",3),Carregamento(3,5,"200*x",3),Carregamento(5,6,30,2,0,0.75)]
+'''carregamentos1 = [ Carregamento(0,3,"100*x",3),Carregamento(3,5,"200*x",3),Carregamento(5,6,30,2,0,0.75)]
 retangulos1=[Retangulo(0,250,0,20),Retangulo(115,135,20,320),Retangulo(0,250,320,340)]
 buracos1=[]
 
 tensao1 = Tensao(retangulos1,buracos1,carregamentos1)
-tensao1.exibe_resultados()
+tensao1.exibe_resultados()'''
 
 
 
-'''#T3
-carregamentos = [ Carregamento(0,1.5,100,1,100),Carregamento(1.5,3,10,2,0,1.5),Carregamento(3,3.75,0,1,100),Carregamento(3.75,4,30,2,0,0),Carregamento(4,5,100,1,10),Carregamento(5,6,30,2,0,0.75)]
+#T3
+carregamentos = [ Carregamento(0,1.5,100,1,100),Carregamento(1.5,3,10,2,0,1.5),Carregamento(3,3.75,"133.3333*x",3),Carregamento(3.75,4,30,2,0,0),Carregamento(4,5,100,1,10),Carregamento(5,6,30,2,0,0.75)]
 retangulos=[Retangulo(0,250,0,20),Retangulo(115,135,20,320),Retangulo(0,250,320,340)]
 buracos=[]
 
 tensao = Tensao(retangulos,buracos,carregamentos)
-tensao.exibe_resultados()'''
+tensao.exibe_resultados()
